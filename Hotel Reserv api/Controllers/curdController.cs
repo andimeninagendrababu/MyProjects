736a5f4824de0a7f504dfb68600e8e357bd1d4ec
Nagendra <andimenibabu98@gmail.com>
2023-01-20 18:46:15 +0530

    intial commit

---
+++ Hotel Reserv api/Controllers/curdController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+using WebApplication4.rep;
+
+namespace WebApplication4.Controllers
+{
+    public class curdController : ApiController
+    {
+        public readonly empInterface1 iemp;
+        public curdController() { }
+        public curdController(empInterface1 edf)
+
+        {
+            this.iemp = edf;
+        }
+        [Route("api/employee/GetAllEmployee")]
+        [HttpGet]
+        public IHttpActionResult GetAllEmployee()
+        {
+            var abc = iemp.GetAllEmployee();
+            if (abc.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(abc);
+        }
+        
+
+        
+        [Route("api/curd/delete/{ID}")]
+        [HttpDelete]
+        public string DeleteEmp(int ID)
+        {
+            return iemp.DeleteEmp(ID);
+        }
+        [Route("api/curd/insert")]
+        [HttpPost]  
+        public HttpResponseMessage Insert(empDetail em)
+        {
+            var abc=iemp.insert(em);
+            return Request.CreateResponse(HttpStatusCode.OK, abc);
+        }
+        [Route("api/curd/updates")]
+        [HttpPost]
+        public  IHttpActionResult Update(empDetail em)
+        {
+            var abc = iemp.Update(em);
+            if(abc==null)
+            {
+                return NotFound();
+            }
+            return Ok("Updated");
+        }
+        [Route("api/Home/bulkinsert")]
+
+        [HttpPost]
+        public HttpResponseMessage bulkinsert(List<empDetail> em)
+        {
+            var abc = iemp.Bulkinsert(em);
+            return Request.CreateResponse(HttpStatusCode.OK, abc);
+        }
+
+        [Route("api/curd/GetEmployeeByID/{ID}")]
+        [HttpGet]
+        
+        public IHttpActionResult GetByEmployeeId(int ID)
+        {
+            var abc = iemp.GetEmployeeByID(ID);
+            if (abc == null)
+            {
+                return NotFound();
+
+            }
+            return Ok(abc);
+        }
+
+    }
+}
+
---
+++ Hotel Reserv api/Controllers/phase1Controller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApplication4.rep;
+
+namespace WebApplication4.Controllers
+{
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
+    public class phase1Controller : ApiController
+    {
+        public readonly phase1Interface1 iemp;
+        public phase1Controller() { }
+
+        public phase1Controller(phase1Interface1 edf)
+
+        {
+            this.iemp = edf;
+        }
+
+        [Route("api/phase1/GetAllDetails")]
+        [HttpGet]
+        public IHttpActionResult GetAllVisitDetails()
+        {
+            var abc = iemp.GetAllDetails();
+            if (abc == null)
+            {
+                return NotFound();
+            }
+            return Ok(abc);
+        }
+
+        [Route("api/phase1/insert")]
+        [HttpPost]
+        public HttpResponseMessage Insert(phase1 em)
+        {
+            var abc = iemp.insert(em);
+            return Request.CreateResponse(HttpStatusCode.OK, abc);
+        }
+
+
+        [Route("api/phase1/updates")]
+        [HttpPost]
+        public IHttpActionResult Update(phase1 em)
+        {
+            var abc = iemp.Update(em);
+            if (abc == null)
+            {
+                return NotFound();
+            }
+            return Ok("Updated");
+        }
+
+        [Route("api/phase1/delete")]
+        [HttpPost]
+        public string DeleteOne([FromBody]int Sl_No)
+        {
+            return iemp.Delete(Sl_No);
+        }
+    }
+}
