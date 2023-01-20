1216198e30adb20ffb3934868cc6eb1ce934fd1d
Nagendra <andimenibabu98@gmail.com>
2023-01-20 19:40:15 +0530

    changes 2

---
+++ web api/Models/phase1Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class phase1Model
+    {
+        public int Sl_No { get; set; }
+        public string Hotel { get; set; }
+        public string Arrival { get; set; }
+        public Nullable<System.DateTime> Departure { get; set; }
+        public string Type { get; set; }
+        public string Guests { get; set; }
+        public Nullable<decimal> price { get; set; }
+    }
+}
---
+++ web api/rep/phase1Interface1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+namespace WebApplication4.rep
+{
+    public interface phase1Interface1
+    {
+        List<phase1Model> GetAllDetails();
+
+        string insert(phase1 em);
+
+        string Update(phase1 em);
+
+        string Delete(int Sl_No);
+    }
+}
